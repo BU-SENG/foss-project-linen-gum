7fb66b3120f6dc9f2d4ef2468059ab0096eb1c39
Bolarinwa David <bolarinwadavid3@gmail.com>
2025-10-27 00:13:24 +0100

    chore(project): unify .gitignore into root file and add inline comments to routes for better clarity

--- frontend/src/layout/RootLayout.jsx
+++ frontend/src/layout/RootLayout.jsx
@@ -1,21 +1,32 @@
 import { Suspense, lazy } from "react";
 import { Outlet } from "react-router-dom";
 
+// Import preloader component
 import Preloader from "../components/Preloader";
+
+// Lazy load Header and Footer components to optimize performance
 const Header = lazy(() => import("../components/Header"));
 const Footer = lazy(() => import("../components/Footer"));
 
+/**
+ * RootLayout component
+ * Acts as the main layout wrapper for all pages.
+ */
 const RootLayout = () => {
   return (
     <>
       <div className="flex flex-col min-h-screen ">
+        {/* Suspense wrapper shows Preloader while Header component loads */}
         <Suspense fallback={<Preloader />}>
           {/* Lazy loaded Header */}
           <Header />
         </Suspense>
+
+        {/* Main content area where nested routes (pages) will render */}
         <main className="grow bg-white dark:bg-gray-900 text-gray-100 transition-colors duration-200">
           <Outlet />
         </main>
+
         <Suspense fallback={<Preloader />}>
           {/* Lazy loaded Footer */}
           <Footer />
