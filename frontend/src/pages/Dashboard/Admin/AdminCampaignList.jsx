9d7eb535f3809b7508121469b2f0fe0f23ccf061
Bolarinwa David <bolarinwadavid3@gmail.com>
2025-11-01 20:02:50 +0100

    feat(dashboard-layout): add sidebar for creator and admin dashboards

---
+++ frontend/src/pages/Dashboard/Admin/AdminCampaignList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AdminCampaignList() {
+  return (
+    <div>
+      <h1 className="text-blue-600">Admin campaign list page</h1>
+    </div>
+  );
+}
+
+export default AdminCampaignList;
