5fd4fdd3c935c53db341b9de7f196fe6646a8993
Bolarinwa David <bolarinwadavid3@gmail.com>
2025-11-01 20:02:50 +0100

    feat(dashboard-layout): add sidebar for creator and admin dashboards

---
+++ frontend/src/pages/Dashboard/DashboardNotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DashboardNotFound() {
+  return (
+    <div>
+      <h1 className="text-blue-700">Dashboard Page Not Found</h1>
+    </div>
+  );
+}
+
+export default DashboardNotFound;
--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -6,8 +6,11 @@ import { Route, createRoutesFromElements } from "react-router-dom";
 import RouteWithAnimation from "../utils/RouteWithAnimation";
 import Preloader from "../components/Preloader";
 
+import DashboardNotFound from "../pages/Dashboard/DashboardNotFound";
+
 // Lazy load all components to improve performance
 const RootLayout = lazy(() => import("../layout/RootLayout"));
+const DashboardLayout = lazy(() => import("../layout/DashboardLayout"));
 
 const Home = lazy(() => import("../pages/Home"));
 const Campaigns = lazy(() => import("../pages/Campaigns"));
@@ -15,6 +18,23 @@ const CampaignDetails = lazy(() => import("../pages/CampaignDetails"));
 const Donate = lazy(() => import("../pages/Donate"));
 const SignUp = lazy(() => import("../pages/Auth/SignUp"));
 const SignIn = lazy(() => import("../pages/Auth/SignIn"));
+
+const CreatorDashboard = lazy(() =>
+  import("../pages/Dashboard/Creator/CreatorDashboard")
+);
+const CreateCampaign = lazy(() =>
+  import("../pages/Dashboard/Creator/CreateCampaign")
+);
+const MyCampaigns = lazy(() =>
+  import("../pages/Dashboard/Creator/MyCampaigns")
+);
+
+const AdminDashboard = lazy(() =>
+  import("../pages/Dashboard/Admin/AdminDashboard")
+);
+const AdminCampaignList = lazy(() =>
+  import("../pages/Dashboard/Admin/AdminCampaignList")
+);
 
 // Define all application routes
 export const routes = createRoutesFromElements(
@@ -61,6 +81,94 @@ export const routes = createRoutesFromElements(
         path="signin"
         element={<RouteWithAnimation Component={SignIn} Fallback={Preloader} />}
       />
+
+      {/* Page not found */}
+      <Route
+        path="*"
+        element={
+          <RouteWithAnimation
+            Component={DashboardNotFound}
+            Fallback={Preloader}
+          />
+        }
+      />
+    </Route>
+
+    {/* Campaign Creator Routes */}
+    <Route>
+      <Route path="/creator/dashboard" element={<DashboardLayout />}>
+        <Route
+          index
+          element={
+            <RouteWithAnimation
+              Component={CreatorDashboard}
+              Fallback={Preloader}
+            />
+          }
+        />
+        <Route
+          path="my-campaigns"
+          element={
+            <RouteWithAnimation Component={MyCampaigns} Fallback={Preloader} />
+          }
+        />
+        <Route
+          path="create-campaign"
+          element={
+            <RouteWithAnimation
+              Component={CreateCampaign}
+              Fallback={Preloader}
+            />
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <RouteWithAnimation
+              Component={DashboardNotFound}
+              Fallback={Preloader}
+            />
+          }
+        />
+      </Route>
+    </Route>
+
+    {/* Admin Routes */}
+    <Route>
+      <Route
+        path="/admin/dashboard"
+        element={<DashboardLayout />}
+        // errorElement={<ErrorFallback />}
+      >
+        <Route
+          index
+          element={
+            <RouteWithAnimation
+              Component={AdminDashboard}
+              Fallback={Preloader}
+            />
+          }
+        />
+        <Route
+          path="all-campaigns"
+          element={
+            <RouteWithAnimation
+              Component={AdminCampaignList}
+              Fallback={Preloader}
+            />
+          }
+        />
+
+        <Route
+          path="*"
+          element={
+            <RouteWithAnimation
+              Component={DashboardNotFound}
+              Fallback={Preloader}
+            />
+          }
+        />
+      </Route>
     </Route>
   </>
 );
